d9a371da7766890fcfc2725a725a967b037716b3
XJJGoing <1499755237@qq.com>
2019-02-27 21:21:02 +0800

    update utils

--- utils/util.js
+++ utils/util.js
@@ -1,4 +1,5 @@
-const formatTime = date => {
+ //获取当前时间的js
+ const formatTime = date => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
   const day = date.getDate()
@@ -9,11 +10,13 @@ const formatTime = date => {
   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
+//就是时间采用的都是两位数字
 const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
 
+//暴露该组件
 module.exports = {
   formatTime: formatTime
 }
