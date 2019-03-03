c7e457095f5465379c228b6a60c09397364d1431
XJJGoing <1499755237@qq.com>
2019-03-03 20:59:55 +0800

    update index.js and person.js and person.wxml

--- pages/person/person.js
+++ pages/person/person.js
@@ -1,30 +1,20 @@
 const app = getApp()
 //引入登录的加载文件
-const logs = require('../logs/logs.js');
+const logs = require('../logs/logs.js')
+let college = this.data.college
+let major = this.data.major
+let number = this.data.number
 Page
 ({
   data:{
-   college:"经济管理学院",
-   major:"信息管理与信息系统",
-   number:"172210409226"
+   college:"",
+   major:"",
+   number:""
   },
-
-  //绑定改变个人信息的函数
-   changemodels:(event)=>{
-    //使用event.target.value拿到点击 按钮事件传输过来的id
-    //form-item1为学院
-    //form-item2为专业
-    //form-item3为学号
-     if (event.target.id === "form-item1"){
-       wx.navigateTo({
-         url: '../changemodels/college/college',
-         data:"学院"
-       })
-     }
-
-  }
-
-
-
+  //加载也秒的时候赋值
+   onLoad(options)
+   {
+     console.log(options.value)
+   },
 })
 
